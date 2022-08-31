91f86fdbbb7d1bfe14ecdd9178a34220dde731ad
Stefanus Nanami <stefanus-nanami@capcom.com>
2022-08-31 12:12:16 +0900

    Changing region color.

--- init.el
+++ init.el
@@ -91,7 +91,7 @@
  '(line-number-minor-tick ((t (:foreground "white"))))
  '(mode-line ((t (:background "dark slate blue" :box nil))))
  '(mode-line-inactive ((t (:background "dark slate blue" :foreground "light slate blue" :box nil))))
- '(region ((t (:extend t :background "midnight blue"))))
+ '(region ((t (:extend t :background "#202050"))))
  '(whitespace-empty ((t (:foreground "firebrick"))))
  '(whitespace-tab ((t (:foreground "gray36")))))
 
