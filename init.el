a9254df4f9eaa1eebd8f9d69198c471817c7fb64
Stefanus Nanami <stefanus-nanami@capcom.com>
2022-09-16 14:56:32 +0900

    Hscroll current line only.

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(auto-hscroll-mode 'current-line)
  '(backup-directory-alist '(("" . "~/.emacs.d/backups")))
  '(c-basic-offset 4)
  '(c-hanging-braces-alist
