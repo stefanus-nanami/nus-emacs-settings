20af8151b632fed49032feac546c1954ade8786c
Stefanus Nanami <stefanus-nanami@capcom.com>
2023-02-28 14:53:57 +0900

    Script for updating emacs using brew.

--- update-emacs.sh
+++ update-emacs.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Tap Emacs Plus.
+brew tap d12frosted/emacs-plus
+
 # Uninstall first.
 brew uninstall emacs-plus
 
-# Install Emacs 29.
-brew install emacs-plus@29 --with-xwidgets --with-imagemagick --with-native-comp --with-poll --with-EmacsIcon4-icon
+# Install Emacs 30.
+brew install emacs-plus@30 --with-xwidgets --with-imagemagick --with-native-comp --with-poll --with-EmacsIcon4-icon
