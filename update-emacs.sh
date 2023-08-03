90a5f89eafa87bcd192e041a04e44c0c30cfc798
Stefanus Nanami <stefanus-nanami@capcom.com>
2023-08-03 17:08:54 +0900

    Update emacs to 29.1.

--- update-emacs.sh
+++ update-emacs.sh
@@ -4,7 +4,7 @@
 brew tap d12frosted/emacs-plus
 
 # Uninstall first.
-brew uninstall emacs-plus
+# brew uninstall emacs-plus
 
-# Install Emacs 30.
-brew install emacs-plus@30 --with-xwidgets --with-imagemagick --with-native-comp --with-poll --with-EmacsIcon4-icon
+# Install Emacs 29.
+brew install emacs-plus@29 --with-xwidgets --with-imagemagick --with-native-comp --with-poll --with-EmacsIcon4-icon
