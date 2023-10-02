05e11d63fa2a4955c8cc4b35c136205a3faa9a03
Stefanus Nanami <stefanus-nanami@capcom.com>
2023-10-02 14:47:09 +0900

    Trying to improve eglot performance.

--- init.el
+++ init.el
@@ -17,18 +17,18 @@
  '(current-language-environment "Japanese")
  '(custom-enabled-themes '(doom-outrun-electric))
  '(custom-safe-themes
-   '("9dccdccfeb236623d5c7cf0250a92308cf307afde4ebdaf173b59e8bbbae1828"
-     "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e"
-     default))
+   '("9dccdccfeb236623d5c7cf0250a92308cf307afde4ebdaf173b59e8bbbae1828" "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e" default))
  '(dabbrev-case-replace nil)
  '(display-fill-column-indicator-column 100)
  '(display-line-numbers-major-tick 50)
  '(display-line-numbers-minor-tick 10)
  '(display-line-numbers-width 5)
  '(dynamic-completion-mode t)
- '(eglot-connect-timeout 180)
+ '(eglot-connect-timeout 300)
+ '(eglot-events-buffer-size 0)
  '(eglot-ignored-server-capabilities '(:inlayHintProvider))
  '(eglot-prefer-plaintext t)
+ '(eglot-sync-connect nil)
  '(electric-pair-mode t)
  '(epa-file-cache-passphrase-for-symmetric-encryption t)
  '(exec-path-from-shell-arguments nil)
