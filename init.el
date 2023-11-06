81b29db3c919ea10fa96544329f565b3ac76de65
Stefanus Nanami <saint_nus@mac.com>
2023-11-07 06:05:40 +0900

    Fixing tree-sitter indent rules for preproc_arg.

--- init.el
+++ init.el
@@ -635,7 +635,8 @@
             (add-to-list 'current-indent-rules '((node-is "case_statement") parent-bol 0))
             (add-to-list 'current-indent-rules '((parent-is "case_statement") parent-bol c-ts-mode-indent-offset))
             (add-to-list 'current-indent-rules '((parent-is "else_clause") parent-bol 0))
-            (add-to-list 'current-indent-rules '((node-is "preproc_arg") column-0 0))
+            (add-to-list 'current-indent-rules '((node-is "preproc_arg") parent-bol c-ts-mode-indent-offset))
+            (add-to-list 'current-indent-rules '((and no-node (parent-is "preproc_arg")) parent-bol 0))
             (setf (alist-get 'cpp treesit-simple-indent-rules) current-indent-rules)))
 
 ;; EGLOT hooks.
