92f9fba707d26ca6a3aa86bb406481445b79ed7c
Stefanus Nanami <saint_nus@mac.com>
2023-12-02 03:10:24 +0900

    Install current version of Emacs.

--- update-emacs.sh
+++ update-emacs.sh
@@ -6,5 +6,5 @@ brew tap d12frosted/emacs-plus
 # Uninstall first.
 # brew uninstall emacs-plus
 
-# Install Emacs 29.
-brew install emacs-plus@29 --with-xwidgets --with-imagemagick --with-native-comp --with-poll --with-EmacsIcon4-icon
+# Install current version of Emacs.
+brew install emacs-plus --with-xwidgets --with-imagemagick --with-native-comp --with-poll --with-EmacsIcon4-icon
